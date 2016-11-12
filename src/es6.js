880864ef73dff607f7c832314b0ea7000f5ace1c
zhongjianyu <hi@zhongjianyu.cn>
2016-11-12 12:25:38 +0800

    init

---
+++ src/es6.js
@@ -0,0 +1,54 @@
+// 'use strict';
+// /**
+// * 输出
+// */
+// function echo (str){
+//     console.log(str);
+// }
+// /**
+// * 字符串处理
+// */
+// let str = `Hi, this is zhongjianyu`;
+
+//  echo (str.charAt(0)); // 'H'
+// echo (str.split('').reverse().join(''));// 'uynaijgnohz si siht ,iH'
+// echo (str.indexOf('i')); // 1
+
+// echo (str.lastIndexOf('i')); // 18
+
+// echo (str.includes('zhongjianyu')); // true
+
+// echo (str.startsWith('Hi')); // true
+
+// echo ('x'.repeat(5)); // xxxxx
+// /**
+// * z
+// * h
+// * o
+// * n
+// * g
+
+// /*for (let char of 'zhong'){
+// echo(char);
+// }*/
+
+// [1,2,...[4,6]]
+
+
+let func = i => i+1
+
+let i = 3;
+
+if (i==3){
+    let i = 4
+}
+
+
+
+
+
+
+
+
+
+
